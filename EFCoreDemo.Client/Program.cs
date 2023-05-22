043874ebf678d3548ad6fbe473be582649dc68e9
Will <doggy.huang@gmail.com>
2023-05-22 13:20:50 +0800

    加入 EFCoreDemo.Client 的 ApiException 例外處理邏輯

--- EFCoreDemo.Client/Program.cs
+++ EFCoreDemo.Client/Program.cs
@@ -1,8 +1,13 @@
 ﻿using EFCoreDemo.Client;
+using Newtonsoft.Json.Linq;
 
 var client = new swaggerClient("https://localhost:5001", new HttpClient());
 
 // All
+
+Console.WriteLine();
+Console.WriteLine("列出所有資料：");
+Console.WriteLine();
 
 var data = await client.GetCourseAllAsync();
 
@@ -13,24 +18,63 @@ foreach (var item in data)
 
 // By Id
 
+Console.WriteLine();
+Console.WriteLine("列出單筆資料：");
+Console.WriteLine();
+
 var course = await client.GetCourseByIdAsync(1);
 
 Console.WriteLine(course.CourseId + "\t" + course.Title);
 
 // Create
 
+Console.WriteLine();
+Console.WriteLine("建立資料：");
+Console.WriteLine();
+
 var newCourse = new CourseCreateDto
 {
     Title = "New Course",
-    Credits = 4,
+    Credits = 6,
     DepartmentId = 1
 };
 
-var result = await client.CreateCourseAsync(newCourse);
+try
+{
+    var result = await client.CreateCourseAsync(newCourse);
+    Console.WriteLine(result.CourseId + "\t" + result.Title);
+}
+catch (EFCoreDemo.Client.ApiException<EFCoreDemo.Client.ProblemDetails> ex)
+{
+    foreach (var (key, value) in ex.Result.AdditionalProperties.ToList())
+    {
+        //Console.WriteLine(key);
+        //Console.WriteLine(value);
+        if (key is "errors")
+        {
+            Console.WriteLine();
+            Console.WriteLine("建立資料發生錯誤：");
+            Console.WriteLine();
 
-Console.WriteLine(result.CourseId + "\t" + result.Title);
+            JObject val = (JObject)value; // { "Credits": [ "課程評價必須介於 1 到 5 之間" ]}
+            foreach (var (fieldName, errors) in val)
+            {
+                JArray errMsgs = (JArray)errors;
+
+                foreach (var errMsg in errMsgs)
+                {
+                    Console.WriteLine($"- {fieldName}\t{errMsg}");
+                }
+            }
+        }
+    }
+    //throw ex;
+}
 
 // All
+Console.WriteLine();
+Console.WriteLine("重新列出所有資料：");
+Console.WriteLine();
 
 var data2 = await client.GetCourseAllAsync();
 
