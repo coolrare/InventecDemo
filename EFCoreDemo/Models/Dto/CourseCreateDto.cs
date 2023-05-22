233d4478f31d7845fa454eb5029f3bd191bbff06
Will <doggy.huang@gmail.com>
2023-05-22 11:34:00 +0800

    模型繫結時加入「輸入驗證」屬性

--- EFCoreDemo/Models/Dto/CourseCreateDto.cs
+++ EFCoreDemo/Models/Dto/CourseCreateDto.cs
@@ -1,10 +1,15 @@
-﻿namespace EFCoreDemo.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreDemo.Models.Dto
 {
     public class CourseCreateDto
     {
+        [Required(ErrorMessage = "請務必填寫課程標題")]
+        [MinLength(3, ErrorMessage = "課程標題至少需要 3 個字元以上")]
         public string Title { get; set; } = null!;
 
-        public int Credits { get; } = 1;
+        [Range(1, 5, ErrorMessage = "課程評價必須介於 1 到 5 之間")]
+        public int Credits { get; set; } = 1;
 
         public int DepartmentId { get; set; }
     }
